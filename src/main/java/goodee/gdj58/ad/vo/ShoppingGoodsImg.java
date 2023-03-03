b4ef71cbc25d43265a36c9377d91299d0102c68a
GDJ <kangjihoon12@naver.com>
2023-03-03 17:46:23 +0900

    [강지훈]ad 수정

--- src/main/java/goodee/gdj58/ad/vo/ShoppingGoodsImg.java
+++ src/main/java/goodee/gdj58/ad/vo/ShoppingGoodsImg.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class ShoppingGoodsImg {
-	private int goodsImgNo;
-	private int goodsNo;
-	private String goodsImgSaveName;
-	private String goodsImgOriginName;
-	private int goodsImgLevel;
-	private String goodsImgType;
-	private String goodsImgSize;
-	private String createdate;
+   private int goodsImgNo;
+   private int goodsNo;
+   private String goodsImgSaveName;
+   private String goodsImgOriginName;
+   private int goodsImgLevel;
+   private String goodsImgType;
+   private String goodsImgSize;
+   private String createdate;
 }
